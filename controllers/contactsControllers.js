69ef17a4bd0ed0269ef9ff2c69062dc8d3d3bee4
DariaPanchuk <daria_panchuk@ukr.net>
2024-03-31 23:13:06 +0300

    solve tasks

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,71 @@
-import contactsService from "../services/contactsServices.js";
+import { nanoid } from 'nanoid';
 
-export const getAllContacts = (req, res) => {};
+import { listContacts, getContactById, removeContact, addContact, updateById } from "../services/contactsServices.js";
+import catchAsync from "../helpers/catchAsync.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = catchAsync(async (req, res) => {
+    const result = await listContacts();
 
-export const deleteContact = (req, res) => {};
+    res.status(200).json({
+        message: "Success!",
+        result,
+    });
+});
 
-export const createContact = (req, res) => {};
+export const getOneContact = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const result = await getContactById(id);
 
-export const updateContact = (req, res) => {};
+    if (!result) {
+        throw HttpError(404, "Not Found");
+    };
+
+    res.status(200).json({
+        message: "Success!",
+        result,
+    });
+});
+
+export const deleteContact = catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const result = await removeContact(id);
+
+    if (!result) {
+        throw HttpError(404, "Not Found");
+    };
+
+    res.status(200).json({
+        message: "Delete Success!",
+        result,
+    });
+});
+
+export const createContact = catchAsync(async (req, res) => {
+    const { name, email, phone } = req.body;
+
+    const result = await addContact(name, email, phone);
+
+    res.status(201).json({
+        message: "Successfully Added!",
+        result,
+    });
+});
+
+export const updateContact = catchAsync(async (req, res) => {
+    if (Object.keys(req.body).length < 1) {
+        throw HttpError(400, "Body must have at least one field");
+    };
+
+    const { id } = req.params;
+    const result = await updateById(id, req.body);
+
+    if (!result) {
+        throw HttpError(404, "Not Found");
+    };
+
+    res.status(200).json({
+        message: "Successfully Updated!",
+        result,
+    });
+});
